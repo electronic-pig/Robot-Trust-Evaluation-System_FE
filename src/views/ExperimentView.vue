ffeb729ee76deffc32124b5330821400f74978bb
liyang <2234333815@qq.com>
2024-04-11 09:49:13 +0800

    完成近红外数据的折现图

--- src/views/ExperimentView.vue
+++ src/views/ExperimentView.vue
@@ -1,38 +1,85 @@
 <template>
-  <div id="lineChart" style="width: 100%;height:600px;"></div>
+  <div id="lineChart" style="width: 100%; height: 600px"></div>
+  <div id="mutilineChart" style="width: 100%; height: 600px"></div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import * as echarts from 'echarts';
-import wonderland from "@/assets/wonderland.json"
+import { onMounted, ref } from "vue";
+import * as echarts from "echarts";
+import * as XLSX from "xlsx";
+import wonderland from "@/assets/wonderland.json";
 
-onMounted(() => {
+onMounted(async () => {
   echarts.registerTheme("customed", wonderland);
-  const myChart = echarts.init(document.getElementById('lineChart'), "customed");
+  const lineChart = echarts.init(
+    document.getElementById("lineChart"),
+    "customed"
+  );
+  const mutilineChart = echarts.init(
+    document.getElementById("mutilineChart"),
+    "customed"
+  );
+  const response = await fetch("/sjs-ED信任度量表+近红外数据+眼动.xlsx");
+  const arrayBuffer = await response.arrayBuffer();
+  const data = new Uint8Array(arrayBuffer);
+  const workbook = XLSX.read(data, { type: "array" });
+  const sheetNames = workbook.SheetNames;
+  const worksheet = workbook.Sheets[sheetNames[1]];
+  const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+    header: ["A"],
+    defval: "",
+  });
 
+  const seriesData = jsonData.map((item) => item.A);
+  const xAxisData = seriesData.map((_, index) => (index / 11).toFixed(2));
   const option = {
     title: {
-      text: '折线图示例'
+      text: "近红外数据",
     },
     tooltip: {
-      trigger: 'axis'
+      trigger: "axis",
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        dataZoom: {
+          yAxisIndex: "none",
+        },
+        dataView: { readOnly: false },
+        magicType: { type: ["line", "bar"] },
+        restore: {},
+        saveAsImage: {},
+      },
     },
     legend: {
-      data: ['销量']
+      data: ["HbO浓度"],
     },
     xAxis: {
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      data: xAxisData,
+      axisLabel: {
+        formatter: "{value} s",
+      },
     },
-    yAxis: {},
-    series: [{
-      name: '销量',
-      type: 'line',
-      data: [820, 932, 901, 934, 1290, 1330, 1320]
-    }]
+    yAxis: {
+      name: "浓度 (mmol/L*mm)",
+    },
+    series: [
+      {
+        name: "HbO浓度",
+        type: "line",
+        data: seriesData,
+      },
+    ],
+    dataZoom: [
+      {
+        type: "slider",
+        start: 95,
+        end: 100,
+      },
+    ],
   };
 
-  myChart.setOption(option);
+  lineChart.setOption(option);
 });
 </script>
 
