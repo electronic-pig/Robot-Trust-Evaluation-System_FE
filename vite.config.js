7798c78eb4e2b9c4f3da94aa641e70a83443072c
liyang <2234333815@qq.com>
2024-04-21 20:08:14 +0800

    🐞 fix(部署): 修改静态资源路径

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
